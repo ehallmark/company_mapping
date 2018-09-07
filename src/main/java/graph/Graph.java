7cd7f8d429410548f4065602e359470fb1e989ae
ehallmark <ehallmark@gttgrp.com>
2018-09-07 10:30:25 -0700

    c

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -7,10 +7,12 @@ import models.Association;
 import models.Model;
 
 import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 public class Graph {
-
+    private static final Lock lock = new ReentrantLock();
 
     private Map<Association.Model, Map<Integer, Node>> nodeCache;
 
@@ -19,86 +21,118 @@ public class Graph {
     }
 
     public void addNode(Association.Model model, int id, Node node) {
-        node.getModel().setNodeCache(this);
-        nodeCache.putIfAbsent(model, new HashMap<>());
-        Map<Integer, Node> resourceMap = nodeCache.get(model);
-        resourceMap.putIfAbsent(id, node);
+        lock.lock();
+        try {
+            node.getModel().setNodeCache(this);
+            nodeCache.putIfAbsent(model, new HashMap<>());
+            Map<Integer, Node> resourceMap = nodeCache.get(model);
+            resourceMap.putIfAbsent(id, node);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void connectNodes(Node node, Node target, Association association) {
-        node.addEdge(new Edge(node, target, association));
+        lock.lock();
+        try {
+            node.addEdge(new Edge(node, target, association));
+        } finally {
+            lock.unlock();
+        }
     }
 
     public Node findNode(Association.Model model, int id) {
-        return nodeCache.getOrDefault(model, Collections.emptyMap()).get(id);
+        lock.lock();
+        try {
+            return nodeCache.getOrDefault(model, Collections.emptyMap()).get(id);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public Node deleteNode(Association.Model model, int id) {
-        return nodeCache.getOrDefault(model, new HashMap<>(1)).remove(id);
+        lock.lock();
+        try {
+            return nodeCache.getOrDefault(model, new HashMap<>(1)).remove(id);
+        } finally {
+            lock.unlock();
+        }
     }
 
     public List<Model> getModelList(Association.Model model) {
-        Map<Integer, Node> idMap = nodeCache.getOrDefault(model, Collections.emptyMap());
-        return idMap.values().stream().map(Node::getModel)
-                .sorted((n1, n2) -> Integer.compare(n1.getId(), n2.getId())).collect(Collectors.toList());
+        lock.lock();
+        try {
+            Map<Integer, Node> idMap = nodeCache.getOrDefault(model, Collections.emptyMap());
+            return idMap.values().stream().map(Node::getModel)
+                    .sorted((n1, n2) -> Integer.compare(n1.getId(), n2.getId())).collect(Collectors.toList());
+        } finally {
+            lock.unlock();
+        }
     }
 
-    @Getter
     private static Graph graph;
     public static Graph load() throws Exception {
         return load(false);
     }
-    public static synchronized Graph load(boolean force) throws Exception {
+    public static Graph load(boolean force) throws Exception {
         if(graph!=null && !force) return graph;
-        // test graph
-        graph = new Graph();
+        lock.lock();
+        try {
+            // test graph
+            graph = new Graph();
 
-        Association.Model[] modelTypes = Association.Model.values();
-        List<Model> allModels = new ArrayList<>();
-        for (Association.Model modelType : modelTypes) {
-            System.out.println("Loading model type: " + modelType);
-            Model model = Main.getModelByType(modelType);
-            List<Model> models = Database.selectAll(model.isRevenueModel(), modelType, model.getTableName(),
-                    model.getAvailableAttributes().stream().filter(f -> f.endsWith("_id")).collect(Collectors.toList()));
-            allModels.addAll(models);
-            for (Model instance : models) {
-                graph.addNode(modelType, instance.getId(), new Node(instance));
+            Association.Model[] modelTypes = Association.Model.values();
+            List<Model> allModels = new ArrayList<>();
+            for (Association.Model modelType : modelTypes) {
+                System.out.println("Loading model type: " + modelType);
+                Model model = Main.getModelByType(modelType);
+                List<Model> models = Database.selectAll(model.isRevenueModel(), modelType, model.getTableName(),
+                        model.getAvailableAttributes().stream().filter(f -> f.endsWith("_id")).collect(Collectors.toList()));
+                allModels.addAll(models);
+                for (Model instance : models) {
+                    graph.addNode(modelType, instance.getId(), new Node(instance));
+                }
             }
-        }
 
-        System.out.println("Adding connections...");
-        // need to add edges
-        for (Model model : allModels) {
-            Node node = graph.findNode(Association.Model.valueOf(model.getClass().getSimpleName()), model.getId());
-            for (Association association : model.getAssociationsMeta()) {
-                switch (association.getType()) {
-                    case ManyToOne: {
-                        // model has the parent id
-                        Integer assocId = (Integer) model.getData().get(association.getParentIdField());
-                        if (assocId != null) {
-                            // try find parent
-                            Node assoc = graph.findNode(association.getModel(), assocId);
-                            graph.connectNodes(node, assoc, association);
-                            Association reverseAssociation = assoc.getModel().getAssociationsMeta()
-                                    .stream().filter(a -> a.getAssociationName().equals(association.getReverseAssociationName()))
-                                    .findAny().orElse(null);
+            System.out.println("Adding connections...");
+            // need to add edges
+            for (Model model : allModels) {
+                Node node = graph.findNode(Association.Model.valueOf(model.getClass().getSimpleName()), model.getId());
+                for (Association association : model.getAssociationsMeta()) {
+                    switch (association.getType()) {
+                        case ManyToOne: {
+                            // model has the parent id
+                            Integer assocId = (Integer) model.getData().get(association.getParentIdField());
+                            if (assocId != null) {
+                                // try find parent
+                                Node assoc = graph.findNode(association.getModel(), assocId);
+                                graph.connectNodes(node, assoc, association);
+                                Association reverseAssociation = assoc.getModel().getAssociationsMeta()
+                                        .stream().filter(a -> a.getAssociationName().equals(association.getReverseAssociationName()))
+                                        .findAny().orElse(null);
 
-                            if (reverseAssociation != null) {
-                                graph.connectNodes(assoc, node, reverseAssociation);
+                                if (reverseAssociation != null) {
+                                    graph.connectNodes(assoc, node, reverseAssociation);
+                                }
                             }
+                            break;
                         }
-                        break;
-                    }
-                    case OneToMany: {
-                        // other model has the parent id
+                        case OneToMany: {
+                            // other model has the parent id
 
-                        break;
-                    }
-                    default: {
-                        throw new RuntimeException("Unsupport join type: " + association.getType());
+                            break;
+                        }
+                        default: {
+                            throw new RuntimeException("Unsupport join type: " + association.getType());
+                        }
                     }
                 }
             }
+        } catch(Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error creating graph: "+e.getMessage());
+        } finally {
+            lock.unlock();
         }
         return graph;
     }
